263a5d6392248c7a5f21d57125770495efc58e5a
Siddharth Jain <siddharthjain2199@gmail.com>
2022-03-03 20:27:02 -0800

    Initial Commit

---
+++ DesignHome/DesignHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignHomeApp.swift
+//  DesignHome
+//
+//  Created by admin on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
