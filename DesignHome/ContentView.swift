6e9b701c8db9d1c118294844bd564a74b336298f
Siddharth Jain <siddharthjain2199@gmail.com>
2022-03-03 22:02:51 -0800

    Commit

--- DesignHome/ContentView.swift
+++ DesignHome/ContentView.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color(red: 1.09, green:0.63, blue:0.52).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                
+                VStack {
+                    
+                    Image("Siddharth pic")
+                        .resizable() .frame(width: 180, height: 180).clipShape(Circle()).overlay(Circle().stroke(Color.white,lineWidth:5))
+                    
+                    Text("Siddharth")
+                        .font(.custom("Savoye LET Plain:1.0", size:60))
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Text("Happy Work Anniversary !!")
+                        .foregroundColor(.white)
+                        .font(.system(size:25))
+                }
+                //Divider()
+                //OveryLay
+                VStack {
+                    RoundedRectangle(cornerRadius: 25).fill(Color.white)
+                        .frame(height:50)
+                        .overlay(HStack{
+                            Image(systemName: "envelope").foregroundColor(.green)
+                            Text("siddharth@gmail.com")}).padding(.all)
+                    
+                    
+                    //Alternative
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25).fill(Color.white)
+                            .frame(height:50)
+                        HStack{
+                            Image(systemName: "phone.fill").foregroundColor(.green)
+                            Text("+91 9123456777")
+                        }
+                        
+                    }.padding(.horizontal)
+                    
+                    
+                    Circle().fill(Color.white).padding(.all,130)
+                        .overlay(Image(systemName: "arrow.forward").resizable().aspectRatio(contentMode: .fit).frame(width: 50, height: 50, alignment: .center))
+                }
+            }
+        }
     }
 }
 
@@ -19,3 +63,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
